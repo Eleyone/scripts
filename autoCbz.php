d0e36c41997c3af21008b340471250361865ffba
arnaud.grousset <arnaud.grousset@prestataire.april.com>
2017-05-24 15:55:28 +0200

    scan dir et file

--- autoCbz.php
+++ autoCbz.php
@@ -5,26 +5,76 @@
 /**
  * Rip les dossiers d'image pour en faire des cbz
  */
-
-echo getcwd() . "\n";
-
 $path = (isset($argv[1])) ? $argv[1] : getcwd();
 
+echo "Path to analyse : {$path}\n";
+
 /**
- * Compresse le dossier
+ * Définition des différent classe utilisé
  */
-function _doCbz($folder) {
-    echo $folder->getfilename()."\n";
-}
-
-
-
-$iterator = new RecursiveDirectoryIterator($path, FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS); 
-foreach($iterator as $file) {
-    if (!$iterator->hasChildren()) {
-        var_dump($iterator->current());
-        //_doCbz($file);
+abstract class FilesystemRegexFilter extends RecursiveRegexIterator {
+    protected $regex;
+    public function __construct(RecursiveIterator $it, $regex) {
+        $this->regex = $regex;
+        parent::__construct($it, $regex);
     }
 }
+
+class FilenameFilter extends FilesystemRegexFilter {
+    // Filter files against the regex
+    public function accept() {
+        return ( ! $this->isFile() || preg_match($this->regex, $this->getFilename()));
+    }
+}
+
+/**
+ * @param $dir
+ *
+ * @return RegexIterator
+ */
+function getImageFromDir($dir) {
+    $filesToAdd = array();
+
+    $Directory = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
+    $filter = new FilenameFilter($Directory, '/\.(?:png|jpg|jpeg)$/');
+
+    $files = new RecursiveIteratorIterator($filter);
+    $files->setMaxDepth(1);
+
+    foreach ($files as $file) {
+        $filesToAdd[] = $file;
+    }
+
+    return $filesToAdd;
+}
+
+
+/**
+ * Fonction qui liste les dossiers contenant des fichiers
+ *
+ * @param $path
+ *
+ * @return array
+ */
+function getDirListToRip($path) {
+    $dirs = array();
+    $dirsInterator = new RecursiveDirectoryIterator($path, FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS);
+    $iterator = new RecursiveIteratorIterator($dirsInterator, RecursiveIteratorIterator::SELF_FIRST);
+
+    foreach($iterator as $file) {
+        if (!$iterator->hasChildren(true)) {
+            $dirs[] = $iterator->getPath();
+        }
+    }
+
+    return array_unique($dirs);
+}
+
+$dirList = getDirListToRip($path);
+
+foreach ($dirList as $dir) {
+    var_dump(getImageFromDir($dir));
+}
+
 
 
